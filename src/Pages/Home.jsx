49957ce3225b97015a2a836fc328f1a4c95627c0
sangleb <sanglesada2@gmail.com>
2025-06-01 10:38:16 +0530

    creatd basic app

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home(){
+    return(
+        <div>
+            {/* Hero Section */}
+            <section className="flex flex-col-reverse justify-center items-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300">
+
+            </section>
+        </div>
+    )
+}
+
+export default Home;
