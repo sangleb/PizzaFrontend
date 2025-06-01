55630a097b0560989b395ead42e88e2836913efa
sangleb <sanglesada2@gmail.com>
2025-06-01 19:35:48 +0530

    added pizza img

---
+++ src/Components/Icons/ArrowRightSolid.jsx
@@ -0,0 +1,21 @@
+export default function ArrowRightSolid() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="2em"
+      height="2em"
+      
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeWidth="2"
+      >
+        <path d="M18 12H5"></path>
+        <path strokeLinejoin="round" d="m15 16l4-4l-4-4"></path>
+      </g>
+    </svg>
+  )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,34 @@
+import PizzaImg from '../assets/Images/pizza.png'
 function Home(){
     return(
         <div>
             {/* Hero Section */}
-            <section className="flex flex-col-reverse justify-center items-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300">
-
+            <section 
+                className="flex flex-col-reverse justify-center items-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300"
+            >
+                <div className="w-4/6 ml-4 text-center md:w-2/6 md:text-left">
+                    <div className="flex justify-center text-4xl md:justify-normal">
+                        <h1 className="pb-5 font-bold text-transparent bg-gradient-to-r from-orange-500 to-orange-300 bg-clip-text">
+                            Enjoy the Slice ... {' '}
+                        </h1>
+                        <h1>
+                            😋👌
+                        </h1>
+                    </div>
+                    <p 
+                        className="pb-4 text-gray-500 font-semibold">
+                            Your Favorite Pizza, Just a Click Away...
+                            From Our Oven to Your Doorstep
+                    </p>
+                    <button 
+                        className="px-4 py-2 text-white bg-orange-500 rounded-md hover:bg-orange-600 justify-center"
+                    >
+                        Order Now
+                    </button>                    
+                </div>
+                <div>
+                    <img src={PizzaImg} alt="pizza image" width={500} height={500} className='rounded-lg' />
+                </div>
             </section>
         </div>
     )
